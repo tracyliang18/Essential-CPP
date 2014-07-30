adc8a1e2b0f071dd2260b6c4ccb5b3d4bbe692d0
tracyliang18 <642098212@qq.com>
2014-07-30 11:49:37 +0800

    add git ignore file

--- Debug/Object-Based-Programming/iterator.d
+++
@@ -1,2 +0,0 @@
-Object-Based-Programming/iterator.d: \
- ../Object-Based-Programming/iterator.cpp
--- Debug/Object-Based-Programming/main.d
+++
@@ -1,4 +0,0 @@
-Object-Based-Programming/main.d: ../Object-Based-Programming/main.cpp \
- ../Object-Based-Programming/stack.h
-
-../Object-Based-Programming/stack.h:
